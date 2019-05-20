3f7ba46541a8565c08ebde6431c38f4d99d655df
TimurSaidov <tim77777@inbox.ru>
2019-05-20 18:01:28 +0300

    Added Service Manager

---
+++ IntermediateTraining/Model/ServiceAssembly.swift
@@ -0,0 +1,19 @@
+//
+//  ServiceAssembly.swift
+//  IntermediateTraining
+//
+//  Created by Timur Saidov on 20/05/2019.
+//  Copyright © 2019 Timur Saidov. All rights reserved.
+//
+
+import Foundation
+
+class ServiceAssembly {
+    
+    
+    // MARK: Public Properties
+    
+    var createController: CreateCompanyViewController {
+        return CreateCompanyViewController()
+    }
+}
